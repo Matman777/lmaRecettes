99f0bcd195bf390c0257a035d87d729867c89e73
Matman777 <mathias.mauer777@gmail.com>
2023-12-07 21:49:41 +0100

    test import js via @vite

--- lmaRecettes/resources/views/recettes.blade.php
+++ lmaRecettes/resources/views/recettes.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./app.css">
-    @vite(['resources/js/fetchGpt.js'])
+    {{-- @vite(['resources/js/fetchGpt.js']) --}}
 
     <title>LMA Recettes</title>
 </head>
@@ -46,7 +46,7 @@
 
         <button type="button" onclick="getRecipe()">Générer ma recette</button>
 
-        {{-- <script src="fetchGpt.js"></script> --}}
+        <script src="fetchGpt.js"></script>
     </div>
 </body>
 
